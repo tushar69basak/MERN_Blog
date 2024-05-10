dd749fa8d2054ec361ce6ae5ffbf17588178196a
tushar69basak <tusharbasak2017@gmail.com>
2024-05-11 00:23:42 +0530

    create a test api route

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 this is readme file
+npm run dev - for run the backend
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGOOSE)
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use('/api/user', userRoutes)
